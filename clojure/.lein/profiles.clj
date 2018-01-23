691fcf424ece15d6f5b62573e1327c0ac7f9e493
Aron Griffis <aron@scampersand.com>
2018-01-23 07:02:53 -0500

    update lein profiles for clojure 1.9.0 compat

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,8 +1,8 @@
-{:user {:plugins [[cider/cider-nrepl "0.14.0"]
-                   [lein-cljfmt "0.5.6"]
-                   [jonase/eastwood "0.2.4"]]
-        :dependencies [[cljfmt "0.5.1"]
-                       [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
+{:user {:plugins [[cider/cider-nrepl "0.16.0"]
+                   [lein-cljfmt "0.5.7"]
+                   [jonase/eastwood "0.2.5"]]
+        :dependencies [[cljfmt "0.5.7"]
+                       [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]
                        [alembic "0.3.2"]]
         :repl-options {:init (require 'cljfmt.core)}}}
 
