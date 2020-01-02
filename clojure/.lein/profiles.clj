80d6748205432f7deffad82b0fd106a65653f870
Aron Griffis <aron@scampersand.com>
2020-01-02 09:04:21 -0500

    clojure: lein version bump, drop cljfmt

--- clojure/.lein/profiles.clj
+++ clojure/.lein/profiles.clj
@@ -1,8 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.16.0"]
-                   [lein-cljfmt "0.5.7"]
-                   [jonase/eastwood "0.2.5"]]
-        :dependencies [[cljfmt "0.5.7"]
-                       [jonase/eastwood "0.2.5" :exclusions [org.clojure/clojure]]
-                       [alembic "0.3.2"]]
-        :repl-options {:init (require 'cljfmt.core)}}}
-
+{:user {:dependencies [[jonase/eastwood "0.3.6" :exclusions [org.clojure/clojure]]
+                       [alembic "0.3.2"]],
+        :plugins [[cider/cider-nrepl "0.22.0"]
+                  [jonase/eastwood "0.3.6"]]}}
