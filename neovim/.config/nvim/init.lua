64b1349640e5525bb00a3ae9f5bbaad0ad0b4375
Aron Griffis <aron@scampersand.com>
2023-07-11 14:51:34 -0400

    switch to lazyvim, default config

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,23 +1,2 @@
--- init.lua
---
--- Written in 2003-2022 by Aron Griffis <aron@arongriffis.com>
--- (originally as .vimrc)
---
--- To the extent possible under law, the author(s) have dedicated all copyright
--- and related and neighboring rights to this software to the public domain
--- worldwide. This software is distributed without any warranty.
---
--- CC0 Public Domain Dedication at
--- http://creativecommons.org/publicdomain/zero/1.0/
---------------------------------------------------------------------------------
-
--- Make my.* available everywhere
-_G.my = require('my')
-
--- Default global settings
-require('settings')
-
--- Plugins loaded via Packer
-require('plugins')
-
--- Post-plugin stuff is in after/plugin/caboose.lua
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
