b277354c919bcde2dde05fb6c88838e809baa7db
Aron Griffis <aron@scampersand.com>
2023-07-14 10:55:27 -0400

    chdir to project root (like old vim-rooter)

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require('lazy').setup {
     { import = 'lazyvim.plugins.extras.lang.typescript' },
     { import = 'lazyvim.plugins.extras.linting.eslint' },
     { import = 'lazyvim.plugins.extras.util.mini-hipatterns' },
+    { import = 'lazyvim.plugins.extras.util.project' },
     -- import/override with your plugins
     { import = 'plugins' },
   },
