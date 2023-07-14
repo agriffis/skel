6788e925e75229b55b351a6629d590e1364745dc
Aron Griffis <aron@scampersand.com>
2023-07-14 12:44:41 -0400

    more jdtls

--- neovim/.config/nvim/lua/plugins/jdtls.lua
+++ neovim/.config/nvim/lua/plugins/jdtls.lua
@@ -1,11 +1,37 @@
 return {
+  -- Add java to treesitter.
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == 'table' then
+        vim.list_extend(opts.ensure_installed, { 'java' })
+      end
+    end,
+  },
+
+  -- Set up lsp with mfussenegger/nvim-jdtls instead of nvim-lspconfig.
   {
     'neovim/nvim-lspconfig',
-    dependencies = { 'mfussenegger/nvim-jdtls' },
+    dependencies = { 'folke/which-key.nvim', 'mfussenegger/nvim-jdtls' },
     opts = {
+      -- make sure mason installs the server
+      servers = {
+        jdtls = {
+          -- stylua: ignore
+          keys = {
+            { '<leader>co', function() require('jdtls').organize_imports() end, desc = 'Organize Imports', },
+            { '<leader>cR', function() require('jdtls').rename_file() end, desc = 'Rename File', },
+            { '<leader>cxv', function() require('jdtls').extract_variable() end, desc = 'Extract Variable', },
+            { '<leader>cxv', function() require('jdtls').extract_variable { visual = true } end, mode = 'v', desc = 'Extract Variable', },
+            { '<leader>cxc', function() require('jdtls').extract_constant() end, desc = 'Extract Constant', },
+            { '<leader>cxc', function() require('jdtls').extract_constant { visual = true } end, mode = 'v', desc = 'Extract Constant', },
+            { '<leader>cxm', function() require('jdtls').extract_method { visual = true } end, mode = 'v', desc = 'Extract Method', },
+          },
+        },
+      },
       setup = {
         -- Override the default jdtls server startup to use nvim-jdtls.
-        ['jdtls'] = function()
+        jdtls = function()
           -- The lspconfig configuration for jdtls contains two useful items:
           -- 1. The list of filetypes on which to match.
           -- 2. Custom method for finding the root for a java project.
@@ -13,8 +39,8 @@ return {
           local find_java_project_root = lsp_config.root_dir
           local filetypes = lsp_config.filetypes
 
-          -- Attach jdtls for the proper filetypes (i.e. java). Existing server will be reused if
-          -- the root_dir matches.
+          -- Attach jdtls for the proper filetypes (i.e. java).
+          -- Existing server will be reused if the root_dir matches.
           vim.api.nvim_create_autocmd('FileType', {
             group = vim.api.nvim_create_augroup('MyJdtls', { clear = true }),
             pattern = filetypes,
@@ -37,8 +63,13 @@ return {
                 cmd = cmd,
                 root_dir = root_dir,
               }
+              require('which-key').register(
+                { c = { x = { name = 'Extract' } } },
+                { prefix = '<leader>' }
+              )
             end,
           })
+
           return true -- avoid duplicate servers
         end,
       },
