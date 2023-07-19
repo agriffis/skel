7353421513347c82555e9861ad079cd063889cd6
Aron Griffis <aron@scampersand.com>
2023-07-19 11:48:01 -0400

    add clojure treesitter, kill eslint

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -22,7 +22,6 @@ require('lazy').setup {
     { import = 'lazyvim.plugins.extras.lang.json' },
     { import = 'lazyvim.plugins.extras.lang.python' },
     { import = 'lazyvim.plugins.extras.lang.typescript' },
-    { import = 'lazyvim.plugins.extras.linting.eslint' },
     { import = 'lazyvim.plugins.extras.util.mini-hipatterns' },
     { import = 'lazyvim.plugins.extras.util.project' },
     -- import/override with your plugins
--- neovim/.config/nvim/lua/plugins/conjure.lua
+++ neovim/.config/nvim/lua/plugins/conjure.lua
@@ -1,6 +1,16 @@
 local sexp_filetypes = { 'clojure', 'lisp', 'scheme' }
 
 return {
+  -- Add clojure to treesitter.
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == 'table' then
+        vim.list_extend(opts.ensure_installed, { 'clojure' })
+      end
+    end,
+  },
+
   { 'tpope/vim-classpath', lazy = true, ft = { 'java', 'clojure' } },
 
   { 'Olical/conjure', lazy = true, ft = { 'clojure' } },
