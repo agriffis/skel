709c4983445aec8f7ef690875d66823632910231
Aron Griffis <aron@scampersand.com>
2023-07-27 18:13:08 -0400

    better fzf bindings with lua

--- neovim/.config/nvim/lua/plugins/fzf.lua
+++ neovim/.config/nvim/lua/plugins/fzf.lua
@@ -1,13 +1,3 @@
-local function search_cword()
-  require('fzf-lua').live_grep {
-    search = vim.fn.expand('<cword>'),
-  }
-end
-
-local function files_cwd()
-  require('fzf-lua').files { cwd = vim.fn.expand('%:p:h') }
-end
-
 return {
   -- Disable telescope in favor of fzf
   { 'nvim-telescope/telescope.nvim', enabled = false },
@@ -46,18 +36,20 @@ return {
         }, ' '),
       },
     },
+    -- stylua: ignore
     keys = {
-      { '<leader>bb', '<cmd>FzfLua buffers<cr>', desc = 'Choose from open buffers' },
-      { '<leader>ff', files_cwd, desc = 'Open file in current dir' },
-      { '<leader>fr', '<cmd>FzfLua oldfiles<cr>', desc = 'Open recent file' },
-      -- This works without needing to find project root because we're always
-      -- in the project root thanks to vim-rooter.
-      { '<c-p>', '<cmd>FzfLua files<cr>', desc = 'Open file in project' },
-      { '<leader>sp', '<cmd>FzfLua live_grep<cr>', desc = 'Search project' },
-      { '<leader>sP', search_cword, desc = 'Search project (current word)' },
-      { '<leader>/', '<cmd>FzfLua live_grep<cr>', desc = 'Search project' },
-      { '<leader>*', search_cword, desc = 'Search project (current word)' },
-      { '<leader>sR', '<cmd>FzfLua resume<cr>', desc = 'Resume last search' },
+      { '<leader>bb', function() require('fzf-lua').buffers() end, desc = 'Choose from open buffers' },
+      { '<leader>ff', function() require('fzf-lua').files { cwd = vim.fn.expand('%:p:h') } end, desc = 'Open file in current dir', },
+      { '<leader>fr', function() require('fzf-lua').oldfiles() end, desc = 'Open file in project', },
+      -- Following work without needing to find project root because we're
+      -- always in the project root thanks to vim-rooter.
+      { '<c-p>', function() require('fzf-lua').files() end, desc = 'Open file in project', },
+      { '<leader>sp', function() require('fzf-lua').live_grep() end, desc = 'Search project', },
+      { '<leader>sP', function() require('fzf-lua').live_grep { search = vim.fn.expand('<cword>'), } end, desc = 'Search project (current word)', },
+      { '<leader>/', function() require('fzf-lua').live_grep() end, desc = 'Search project' },
+      { '<leader>?', function() require('fzf-lua').live_grep { cwd = vim.fn.expand('%:p:h') } end, desc = 'Search current dir' },
+      { '<leader>*', function() require('fzf-lua').live_grep { search = vim.fn.expand('<cword>'), } end, desc = 'Search project (current word)', },
+      { '<leader>sR', function() require('fzf-lua').resume() end, desc = 'Resume last search' },
     },
   },
 }
